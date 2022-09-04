fadd960fcbac77154c5bf5a5348a6c2d103802e6
Bunas74 <90347664+Bunas74@users.noreply.github.com>
2022-09-04 15:47:33 +0300

    Update Counter.java

--- src/by/epam/task7/solution5/Counter.java
+++ src/by/epam/task7/solution5/Counter.java
@@ -2,32 +2,32 @@ package by.epam.task7.solution5;
 
 public class Counter {
 	
-private int count;
+	private int count;
     
-    Counter() {     
-        count = 0;       
-    }
+   	 Counter() {     
+       		 count = 0;       
+   	 }
     
-    Counter(int c) {      
-        count = c;       
-    } 
+   	 Counter(int c) {      
+        	count = c;       
+   	 } 
     
-    public void setCount(int c) {
-        count = c;
-    }
+   	 public void setCount(int c) {
+       		 count = c;
+   	 }
     
-    public int getCount() {
-        return count;
-    }
+   	 public int getCount() {
+        	return count;
+  	  }
     
-    public int countPlus() {
-        count++;
-        return count;
-    }
+   	 public int countPlus() {
+       		 count++;
+       		 return count;
+   	 }
     
-    public int countMinus() {
-        count--;
-        return count;
-    }
+   	 public int countMinus() {
+       		 count--;
+       		 return count;
+    	}
 
 }
