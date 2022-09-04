7bf1e7fde92df35ac074079be4253eac136f55bc
Bunas74 <90347664+Bunas74@users.noreply.github.com>
2022-09-04 15:55:19 +0300

    Update Solution6.java

--- src/by/epam/task7/solution6/Solution6.java
+++ src/by/epam/task7/solution6/Solution6.java
@@ -12,41 +12,41 @@ public class Solution6 {
 		 * поле устанавливается в значение 0. Создать методы изменения времени на заданное
 		 * количество часов, минут и секунд. */
 		
-        Time t = new Time();
+        	Time t = new Time();
         
-        System.out.println("Clock setting menu.");
-        System.out.println("Enter:");
-        System.out.println("\t1 - setting hours; \n\t2 - setting minutes;  \n\t3 - setting seconds;  \n\t4 - viewing the current time;  \n\t0 - exit from the program.");
+        	System.out.println("Clock setting menu.");
+        	System.out.println("Enter:");
+        	System.out.println("\t1 - setting hours; \n\t2 - setting minutes;  \n\t3 - setting seconds;  \n\t4 - viewing the current time;  \n\t0 - exit from the program.");
         
-        try(Scanner sc = new Scanner(System.in)) {
+        	try(Scanner sc = new Scanner(System.in)) {
             
-            int enter;
+            		int enter;
             
-            while(true) {
+            		while(true) {
                 
-                System.out.print("Enter > ");
-                enter = sc.nextInt();
+                		System.out.print("Enter > ");
+                		enter = sc.nextInt();
                 
-                if(enter == 1) {
-                    System.out.print("Hours > ");
-                    t.setHours(sc.nextInt());
-                }
-                else if(enter == 2) {
-                    System.out.print("Minutes > ");
-                    t.setMinutes(sc.nextInt());
-                }
-                else if(enter == 3) {
-                    System.out.print("Seconds > ");
-                    t.setSeconds(sc.nextInt());
-                }
-                else if(enter == 4) {
-                    t.printTime();
-                }
-                else if(enter == 0) {
-                    System.out.print("Good luck.");
-                    return;
-                }
-            }
-        }
+                		if(enter == 1) {
+                    			System.out.print("Hours > ");
+                    			t.setHours(sc.nextInt());
+                		}
+                		else if(enter == 2) {
+                    			System.out.print("Minutes > ");
+                    			t.setMinutes(sc.nextInt());
+                		}
+                		else if(enter == 3) {
+                    			System.out.print("Seconds > ");
+                    			t.setSeconds(sc.nextInt());
+                		}
+                		else if(enter == 4) {
+                    			t.printTime();
+                		}
+                		else if(enter == 0) {
+                    			System.out.print("Good luck.");
+                    			return;	
+                		}
+            		}
+        	}
 	}
 }
