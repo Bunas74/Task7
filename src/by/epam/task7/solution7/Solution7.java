f984d3bd7d5b3979fef82f5409c0e57dc6b29cb1
Bunas74 <90347664+Bunas74@users.noreply.github.com>
2022-09-04 15:57:05 +0300

    Update Solution7.java

--- src/by/epam/task7/solution7/Solution7.java
+++ src/by/epam/task7/solution7/Solution7.java
@@ -7,14 +7,14 @@ public class Solution7 {
 		/*7. Описать класс, представляющий треугольник. Предусмотреть методы для
 		 *создания объектов, вычисления площади, периметра и точки пересечения медиан. */
         
-        Triangle t = new Triangle(5, 6, 7);
+        	Triangle t = new Triangle(5, 6, 7);
         
-        System.out.println("Side of triangle is:");
-        t.printSide();
+        	System.out.println("Side of triangle is:");
+        	t.printSide();
         
-        System.out.println("Perimeter of triangle is: " + t.perimeter());
+        	System.out.println("Perimeter of triangle is: " + t.perimeter());
         
-        System.out.printf("Area of triangle is: %.2f", t.area());
+        	System.out.printf("Area of triangle is: %.2f", t.area());
 
 	}
 }
